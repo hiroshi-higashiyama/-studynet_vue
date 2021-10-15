ad8d7b30d0804d48fa44384cf320177378647ac6
hiroshi <s20840011@gmail.com>
2021-10-16 07:09:14 +0900

    Course.vue api修正

--- src/views/Course.vue
+++ src/views/Course.vue
@@ -14,9 +14,8 @@
 
             <ul>
               <li v-for="lesson in lessons" v-bind:key="lesson.id">
-                <a @click="activeLesson = lesson">{{ lesson.title }}</a>
-                </li>
-
+                <a @click="setActiveLesson(lesson)">{{ lesson.title }}</a>
+              </li>
             </ul>
           </div>
 
@@ -24,8 +23,52 @@
             <template v-if="$store.state.user.isAuthenticated">
               <template v-if="activeLesson">
                 <h2>{{ activeLesson.title }}</h2>
-                
+
                 {{ activeLesson.long_description }}
+
+                <hr />
+
+                <article
+                  class="media box"
+                  v-for="comment in comments"
+                  v-bind:key="comment.id"
+                >
+                  <div class="media-content">
+                    <div class="content">
+                      <p>
+                        <strong>{{ comment.name }}</strong>
+                        {{ comment.created_at }}
+                        <br />
+                        {{ comment.content }}
+                      </p>
+                    </div>
+                  </div>
+                </article>
+
+                <form v-on:submit.prevent="submitComment()">
+                  <div class="field">
+                    <label class="label">Name</label>
+                    <div class="control">
+                      <input type="text" class="input" v-model="comment.name" />
+                    </div>
+                  </div>
+
+                  <div class="field">
+                    <label class="label">Content</label>
+                    <div class="control">
+                      <textarea
+                        class="textarea"
+                        v-model="comment.content"
+                      ></textarea>
+                    </div>
+                  </div>
+
+                  <div class="field">
+                    <div class="control">
+                      <button class="button is-link">Submit</button>
+                    </div>
+                  </div>
+                </form>
               </template>
 
               <template v-else>
@@ -53,7 +96,12 @@ export default {
     return {
       course: {},
       lessons: [],
+      comments: [],
       activeLesson: null,
+      comment: {
+        name: "",
+        content: "",
+      },
     };
   },
   mounted() {
@@ -65,8 +113,44 @@ export default {
       console.log(response.data);
 
       this.course = response.data.course;
-      this.lessons = response.data.lessons
+      this.lessons = response.data.lessons;
     });
+  },
+  methods: {
+    submitComment() {
+      console.log("submitComment");
+
+      axios
+        .post(
+          `/api/v1/courses/${this.course.slug}/${this.activeLesson.slug}/`,
+          this.comment
+        )
+        .then((response) => {
+          this.comment.name = "";
+          this.comment.content = "";
+
+          alert("The comment was added!");
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    setActiveLesson(lesson) {
+      this.activeLesson = lesson;
+
+      this.getComments();
+    },
+    getComments() {
+      axios
+        .get(
+          `/api/v1/courses/${this.course.slug}/${this.activeLesson.slug}/get-comments/`
+        )
+        .then((response) => {
+          console.log(response.data);
+
+          this.comments = response.data;
+        });
+    },
   },
 };
 </script>
